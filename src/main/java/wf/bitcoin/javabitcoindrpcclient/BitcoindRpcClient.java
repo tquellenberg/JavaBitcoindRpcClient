1f2ee41ab320c5a307aa64e245db4ccf70c5195f
Thomas Diesler <tdiesler@redhat.com>
2018-07-25 10:47:14 +0200

    [resolves #64] Add support for listlockunspent

--- src/main/java/wf/bitcoin/javabitcoindrpcclient/BitcoindRpcClient.java
+++ src/main/java/wf/bitcoin/javabitcoindrpcclient/BitcoindRpcClient.java
@@ -495,6 +495,13 @@ public interface BitcoindRpcClient {
     String connected();
   }
 
+  static interface LockedUnspent extends Serializable {
+  
+    String txId();
+    
+    int vout();
+  }
+  
   static interface TxOut extends Serializable {
 
     String bestBlock();
@@ -902,6 +909,13 @@ public interface BitcoindRpcClient {
     int confirmations();
   }
 
+  /**
+   * The listlockunspent RPC returns a list of temporarily unspendable (locked) outputs.
+   * 
+   * @see <a href="https://bitcoin.org/en/developer-reference#listlockunspent">listlockunspent</a>
+   */
+  List<LockedUnspent> listLockUnspent();
+  
   /**
    * The listreceivedbyaddress RPC lists the total number of bitcoins received by each address.
    * 
